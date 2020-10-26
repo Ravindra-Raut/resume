d946dd66a7aab2eaa082cc6e6505d57cfd50eec2
EricFletcher1 <64165327+EverythingIsAwesome2009@users.noreply.github.com>
2020-10-26 19:51:46 -0400

    update commit

---
+++ start.R
@@ -0,0 +1,11 @@
+# install.packages("devtools")
+devtools::install_github("nstrayer/datadrivencv", force = TRUE)
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Eric S. Fletcher",
+  data_location = "https://docs.google.com/spreadsheets/d/1z7MbTZ2VDxFnMxAeEYk6Y8XQdxSfNAhNgFpIMZwE6Wc/edit#gid=917338460",
+  pdf_location = "https://github.com/iamericfletcher/resume",
+  html_location = "https://github.com/iamericfletcher/resume",
+  source_location = "https://github.com/iamericfletcher/resume"
+)
+
