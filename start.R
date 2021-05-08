edf0c10cafa6b67f065f2f984ae304c93ff39861
Ravindra Raut <33831817+Ravindra-Raut@users.noreply.github.com>
2021-05-08 19:47:18 +0530

    Update start.R

--- start.R
+++ start.R
@@ -2,10 +2,10 @@
 devtools::install_github("nstrayer/datadrivencv", force = TRUE)
 
 datadrivencv::use_datadriven_cv(
-  full_name = "Eric S. Fletcher",
-  data_location = "https://docs.google.com/spreadsheets/d/1z7MbTZ2VDxFnMxAeEYk6Y8XQdxSfNAhNgFpIMZwE6Wc/edit#gid=917338460",
-  pdf_location = "https://github.com/iamericfletcher/resume",
-  html_location = "https://github.com/iamericfletcher/resume",
-  source_location = "https://github.com/iamericfletcher/resume"
+  full_name = "Ravindra raut",
+  data_location = "https://docs.google.com/spreadsheets/d/1vSZcNnmwqgQBbtCp1SAwGIUB8cqqa3y6VgNqH3C6Jv8/edit#gid=0",
+  pdf_location = "https://github.com/Ravindra-Raut/resume",
+  html_location = "https://github.com/Ravindra-Raut/resume",
+  source_location = "https://github.com/Ravindra-Raut/resume"
 )
 
